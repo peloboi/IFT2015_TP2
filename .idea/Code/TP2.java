7665d4422ca1c7d45cdd36b6d5284efcc5cba712
Peloboi <vincent.peloquin@hotmail.com>
2023-07-06 20:44:28 -0400

    second

--- .idea/Code/TP2.java
+++ .idea/Code/TP2.java
@@ -1,3 +1,15 @@
+// Auteurs:
+// TIRAO, Raihiti - 20235290
+// PELOQUIN, Vincent - 20105029
+
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TP2 {
     public static void main(String[] args){
 
@@ -11,6 +23,16 @@ public class TP2 {
         try {
             Scanner read = new Scanner(readFile);
             String rawInput = "";
+
+            while (read.hasNextLine()){
+                rawInput += read.nextLine();
+            }
+
+            String[] commands = rawInput.split(";", 0);
+
+            for (int i=0; i < commands.length ; i++){
+                System.out.println(commands[i]);
+            }
 
 
         }
